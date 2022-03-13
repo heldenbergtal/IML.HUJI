07211af8eb1eb794e93fdd3a20df1cea39a753c4
talheldenberg <tal.heldenerg@mail.huji.ac.il>
2022-03-13 22:31:15 +0200

    complete questions 1,2

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -1,16 +1,47 @@
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
+from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
+
+X = np.random.normal(10, 1, 1000)  
 
 
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+
+    # m = 1000
+    global X
+
+    fitted_X = UnivariateGaussian()
+    fitted_X.fit(X)
+    print(f"({fitted_X.mu_}, {fitted_X.var_})")
+
+    fig = make_subplots(rows=5, cols=1, specs=[[{"rowspan": 4, "secondary_y": True}], [None], [None], [None], [{}]]) \
+        .add_trace(go.Histogram(x=X, opacity=0.75, bingroup=1, nbinsx=200, marker_color='navy'), secondary_y=False)
+    fig.update_layout(title_text="$\\text{ Histograms of Sample }X\\sim\\mathcal{N}\\left(10, 1\\right)$") \
+        .update_yaxes(title_text="Number of occurrences", secondary_y=False, row=1, col=1) \
+        .update_xaxes(title_text="Values", row=1, col=1)
+    fig.show()
 
     # Question 2 - Empirically showing sample mean is consistent
-    raise NotImplementedError()
+    ms = np.linspace(10, 1000, 200).astype(int)
+    estimated_mean = []
+    fitted_X = UnivariateGaussian()
+    for m in ms:
+        fitted_X.fit(X[:m])
+        distance = np.absolute(10 - fitted_X.mu_)
+        estimated_mean.append(distance)
+
+    go.Figure([go.Scatter(x=ms, y=estimated_mean, mode='markers+lines', name=r'$distance$')],
+              layout=go.Layout(title=r"$\text{Absolute Error of the Estimator as Function of Number of Samples}$",
+                               xaxis_title="$m\\text{ - number of samples}$",
+                               yaxis_title="r$distance$",
+                               height=400)).show()
+
+
 
     # Question 3 - Plotting Empirical PDF of fitted model
     raise NotImplementedError()
