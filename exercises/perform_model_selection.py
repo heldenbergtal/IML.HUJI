2d78220d78ad6eb9cdca76b3468bd6bea0ef34ad
talheldenberg <tal.heldenerg@mail.huji.ac.il>
2022-06-16 12:20:23 +0300

    before pulling for ex6

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import numpy as np
 import pandas as pd
+import sklearn.linear_model
 from sklearn import datasets
 from IMLearn.metrics import mean_square_error
 from IMLearn.utils import split_train_test
@@ -53,7 +54,8 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
                                        test_y.to_numpy().reshape(len(test_y))
 
     fig1 = go.Figure()
-    fig1.add_trace(go.Scatter(x=X, y=noiseless_y, mode="markers+lines"))
+    fig1.add_trace(
+        go.Scatter(x=X, y=noiseless_y, mode="markers+lines", name="real"))
     fig1.add_trace(go.Scatter(x=test_x, y=test_y,
                               mode="markers", marker=dict(color='#ff6361'),
                               name="test"))
@@ -66,27 +68,34 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
     polynomial_degrees = 11
-    validation_error = []
+    validation_error, training_error = [], []
     for deg in range(polynomial_degrees):
         ts, vs = cross_validate(PolynomialFitting(deg), train_x, train_y,
                                 mean_square_error)
         validation_error.append(vs)
+        training_error.append(ts)
 
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=[i for i in range(polynomial_degrees)],
                               y=validation_error,
-                              mode="markers"))
+                              mode="markers", marker=dict(color='#ff6361'),
+                              name="validation"))
+    fig2.add_trace(go.Scatter(x=[i for i in range(polynomial_degrees)],
+                              y=training_error,
+                              mode="markers", marker=dict(color='#58508d'),
+                              name="training"))
     fig2.update_layout(
-        title="Mean Validation Error as Function of Polynom Degree",
+        title="Mean Error as Function of Polynom Degree",
         xaxis_title="degree",
         yaxis_title="validation error", height=700, width=1000)
     fig2.show()
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
     k_star = np.argmin(validation_error)
+    print("k-star: ", k_star)
     p_reg = PolynomialFitting(k_star).fit(train_x, train_y)
     loss = p_reg.loss(test_x, test_y)
-    print(loss)
+    print("loss on test set: ", loss)
 
 
 def select_regularization_parameter(n_samples: int = 50,
@@ -106,18 +115,14 @@ def select_regularization_parameter(n_samples: int = 50,
     # Question 6 - Load diabetes dataset and split into training and testing portions
     data = datasets.load_diabetes()
     X, y = data.data, data.target
-    train_proportion = 50 / len(y)
-    train_x, train_y, test_x, test_y = split_train_test(pd.DataFrame(X),
-                                                        pd.Series(y),
-                                                        train_proportion=train_proportion)
-    train_x, train_y, test_x, test_y = train_x.to_numpy(), train_y.to_numpy(), test_x.to_numpy(), test_y.to_numpy()
+    train_x, train_y, test_x, test_y = X[:50], y[:50], X[50:], y[50:]
 
     # Question 7 - Perform CV for different values of the regularization parameter for Ridge and Lasso regressions
     lamds_range = np.linspace(0, 1, n_evaluations)
     validation_error_lasso, validation_error_ridge = [], []
     training_error_lasso, training_error_ridge = [], []
     for lam in lamds_range:
-        lasso = LassoRegression(lam, optimizer=GradientDescent)
+        lasso = sklearn.linear_model.Lasso(lam)
         ridge = RidgeRegression(lam)
         ts_lasso, vs_lasso = cross_validate(lasso, train_x, train_y,
                                             mean_square_error)
@@ -148,7 +153,7 @@ def select_regularization_parameter(n_samples: int = 50,
     fig7.add_trace(go.Scatter(x=lamds_range,
                               y=training_error_ridge,
                               mode="markers+lines",
-                              marker=dict(color='#003f5c'), name="validation"),
+                              marker=dict(color='#003f5c'), name="training"),
                    row=1, col=2)
     fig7.update_layout(
         title="Scoring in Different Models",
@@ -161,7 +166,7 @@ def select_regularization_parameter(n_samples: int = 50,
     best_lasso = lamds_range[np.argmin(validation_error_lasso)]
     print(f"regularization best: ridge-{best_ridge}, lasso-{best_lasso}")
 
-    lasso = LassoRegression(lam=best_lasso, optimizer=GradientDescent)
+    lasso = sklearn.linear_model.Lasso(best_lasso)
     ridge = RidgeRegression(lam=best_ridge)
     linear = LinearRegression()
 
@@ -172,12 +177,13 @@ def select_regularization_parameter(n_samples: int = 50,
     lasso_loss = mean_square_error(lasso.predict(test_x), test_y)
     ridge_loss = mean_square_error(ridge.predict(test_x), test_y)
     linear_loss = linear.loss(test_x, test_y)
-    print(f"Models errors: ridge-{ridge_loss}, lasso-{lasso_loss}, linear-{linear_loss}")
+    print(
+        f"Models errors: ridge-{ridge_loss}, lasso-{lasso_loss}, linear-{linear_loss}")
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    # select_polynomial_degree()
-    # select_polynomial_degree(noise=0)
-    # select_polynomial_degree(n_samples=1500, noise=10)
+    select_polynomial_degree()
+    select_polynomial_degree(noise=0)
+    select_polynomial_degree(n_samples=1500, noise=10)
     select_regularization_parameter()
